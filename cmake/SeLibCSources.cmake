9691bac9ed4b4eef3657ea44d3de5e1618403d4e
Samuel Schuepbach <samuel.schuepbach@sourceengineers.com>
2020-01-23 15:58:22 +0100

    Updated cmake build system

--- cmake/SeLibCSources.cmake
+++ cmake/SeLibCSources.cmake
@@ -3,7 +3,7 @@ set(SE_LIB_C_SOURCES
 		${SE_LIB_C_SOURCE_DIR}/container/ByteRingBuffer.c
 		${SE_LIB_C_SOURCE_DIR}/container/FloatRingBuffer.c
 		${SE_LIB_C_SOURCE_DIR}/container/IntRingBuffer.c
-		${SE_LIB_C_SOURCE_DIR}/logger/Logger.c
+		#${SE_LIB_C_SOURCE_DIR}/logger/Logger.c
 		${SE_LIB_C_SOURCE_DIR}/logger/LoggerBuilder.c
 		${SE_LIB_C_SOURCE_DIR}/monitoring/MaxCheck.c
 		${SE_LIB_C_SOURCE_DIR}/monitoring/MinCheck.c
--- test/cmake/UnitTestFiles.cmake
+++ test/cmake/UnitTestFiles.cmake
@@ -5,7 +5,7 @@ set(SE_LIB_C_MOCKS
 
 set(SE_LIB_C_UNIT_TESTS
         ${SE_LIB_C_TEST_DIR}/container/FloatRingBuffer-test.cpp
-        ${SE_LIB_C_TEST_DIR}/logger/LoggerFactory-test.cpp
+        #${SE_LIB_C_TEST_DIR}/logger/LoggerFactory-test.cpp
         ${SE_LIB_C_TEST_DIR}/monitoring/TestMaxCheck.cpp
         ${SE_LIB_C_TEST_DIR}/monitoring/TestMinCheck.cpp
         ${SE_LIB_C_TEST_DIR}/stream/Bytestream-test.cpp
