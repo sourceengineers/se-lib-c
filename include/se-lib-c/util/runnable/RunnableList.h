011ab4f6899ca7d786012c71e9ac02c21bfe43b1
Samuel Schuepbach <samuel.schuepbach@sourceengineers.com>
2021-03-29 17:09:06 +0200

    Added license headers

--- include/se-lib-c/util/runnable/RunnableList.h
+++ include/se-lib-c/util/runnable/RunnableList.h
@@ -3,6 +3,20 @@
  *
  * @copyright    © by Source Engineers GmbH, 2019
  *
+ * @license      This file is part of SE-Lib-C.
+ *
+ *               SE-Lib-C is free software; you can redistribute it and/or
+ *               modify it under the terms of the GNU General Public License as
+ *               published by the Free Software Foundation.
+ *
+ *               SE-Lib-C is distributed in the hope that it will be useful,
+ *               but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *               MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *               GNU General Public License for more details.
+ *
+ *               You should have received a copy of the GNU General Public License
+ *               along with SE-Lib-C.  If not, see <http://www.gnu.org/licenses/>.
+ *
  * @authors      Flurin Bühler <flurin.buehler@sourceengineers.com>
  *
  * @brief        TODO
