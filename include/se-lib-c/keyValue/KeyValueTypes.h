92ebfa891f8473bb781af6033d0b40734e3a09af
Samuel Schuepbach <schuepbs@gmail.com>
2021-04-29 11:34:59 +0200

    Updated license headers

--- include/se-lib-c/keyValue/KeyValueTypes.h
+++ include/se-lib-c/keyValue/KeyValueTypes.h
@@ -1,21 +1,27 @@
 /*!****************************************************************************************************************************************
  * @file         KeyValueTypes.h
  *
- * @copyright    © by Source Engineers GmbH, 2020
+ * @copyright    Copyright (c) 2021 by Source Engineers GmbH. All Rights Reserved.
  *
- * @license      This file is part of SE-Lib-C.
+ * @license {    This file is part of se-lib-c.
  *
- *               SE-Lib-C is free software; you can redistribute it and/or
- *               modify it under the terms of the GNU General Public License as
- *               published by the Free Software Foundation.
+ *               se-lib-c is free software; you can redistribute it and/or
+ *               modify it under the terms of the GPLv3 General Public License Version 3
+ *               as published by the Free Software Foundation.
  *
- *               SE-Lib-C is distributed in the hope that it will be useful,
+ *               se-lib-c is distributed in the hope that it will be useful,
  *               but WITHOUT ANY WARRANTY; without even the implied warranty of
  *               MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *               GNU General Public License for more details.
  *
- *               You should have received a copy of the GNU General Public License
- *               along with SE-Lib-C.  If not, see <http://www.gnu.org/licenses/>.
+ *               You should have received a copy of the GPLv3 General Public License Version 3
+ *               along with se-lib-c.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *               In closed source or commercial projects, GPLv3 General Public License Version 3
+ *               is not valid. In this case the commercial license received with the purchase
+ *               is applied (See SeScopeLicense.pdf).
+ *               Please contact us at scope@sourceengineers.com for a commercial license.
+ * }
  *
  * @authors      Tobias Jäggi <tobias.jaeggi@sourceengineers.com>
  *
--- include/se-lib-c/stream/BufferedFloatStream.h
+++ include/se-lib-c/stream/BufferedFloatStream.h
@@ -1,23 +1,29 @@
 /*!*****************************************************************************
  * @file         BufferedFloatStream.h
  *
- * @copyright    Copyright (c) 2018 by Sourceengineers. All Rights Reserved.
+ * @copyright    Copyright (c) 2021 by Source Engineers GmbH. All Rights Reserved.
  *
- * @license      This file is part of SE-Lib-C.
+ * @license {    This file is part of se-lib-c.
  *
- *               SE-Lib-C is free software; you can redistribute it and/or
- *               modify it under the terms of the GNU General Public License as
- *               published by the Free Software Foundation.
+ *               se-lib-c is free software; you can redistribute it and/or
+ *               modify it under the terms of the GPLv3 General Public License Version 3
+ *               as published by the Free Software Foundation.
  *
- *               SE-Lib-C is distributed in the hope that it will be useful,
+ *               se-lib-c is distributed in the hope that it will be useful,
  *               but WITHOUT ANY WARRANTY; without even the implied warranty of
  *               MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *               GNU General Public License for more details.
  *
- *               You should have received a copy of the GNU General Public License
- *               along with SE-Lib-C.  If not, see <http://www.gnu.org/licenses/>.
+ *               You should have received a copy of the GPLv3 General Public License Version 3
+ *               along with se-lib-c.  If not, see <http://www.gnu.org/licenses/>.
  *
- * @authors      Samuel Schuepbach samuel.schuepbach@sourceengineers.com
+ *               In closed source or commercial projects, GPLv3 General Public License Version 3
+ *               is not valid. In this case the commercial license received with the purchase
+ *               is applied (See SeScopeLicense.pdf).
+ *               Please contact us at scope@sourceengineers.com for a commercial license.
+ * }
+ *
+ * @authors      Samuel Schuepbach <samuel.schuepbach@sourceengineers.com>
  *
  * @brief        Implementation of the float stream interface.
  *               The buffered float stream uses a ring buffer as underlying data
